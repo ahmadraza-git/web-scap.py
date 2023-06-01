80ab2e58e039f2c9ad9e524bf5c8c414b40181e1
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 11:19:59 +0500

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
 import streamlit as st
-
-
-app = Flask(__name__)
-
+from bs4 import BeautifulSoup
+import requests
 def scrape_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -12,16 +8,13 @@ def scrape_data(url):
     paragraph = soup.find('p').text
     return title, paragraph
 
-@app.route('/', methods=['GET', 'POST'])
-def scrape():
-    if request.method == 'POST':
-        url = request.form['url']
-        if url:
-            title, paragraph = scrape_data(url)
-            return render_template('result.html', title=title, paragraph=paragraph)
-        else:
-            return "Please enter a valid URL."
-    return render_template('index.html')
+st.title("Data Scraping App")
+url = st.text_input("Enter the URL to scrape")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Scrape"):
+    if url:
+        title, paragraph = scrape_data(url)
+        st.write(f"Title: {title}")
+        st.write(f"Paragraph: {paragraph}")
+    else:
+        st.warning("Please enter a valid URL.")
