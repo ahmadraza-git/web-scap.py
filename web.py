8b03f0a538c459771a69bf4e12ea3abf2cd31c6a
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-05-31 22:28:41 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,20 +1,31 @@
 import streamlit as st
 from bs4 import BeautifulSoup
 import requests
-def scrape_data(url):
+
+
+def scrape_news():
+    # Send a GET request to the website
+    url = "https://www.example.com/news"
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.string
-    paragraph = soup.find('p').text
-    return title, paragraph
-
-st.title("Data Scraping App")
-url = st.text_input("Enter the URL to scrape")
-
-if st.button("Scrape"):
-    if url:
-        title, paragraph = scrape_data(url)
-        st.write(f"Title: {title}")
-        st.write(f"Paragraph: {paragraph}")
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Create a BeautifulSoup object to parse the HTML content
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        # Find the HTML elements containing the news articles
+        articles = soup.find_all("div", class_="article")
+        
+        # Extract the title and URL for each article
+        for article in articles:
+            title = article.find("h2").text.strip()
+            link = article.find("a")["href"]
+            
+            print("Title:", title)
+            print("URL:", link)
+            print("------")
     else:
-        st.warning("Please enter a valid URL.")
+        print("Failed to retrieve news.")
+
+# Call the function to start scraping
+scrape_news()
