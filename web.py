90bc0f6cdf59f6c2365c8cb227161f088befa295
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-05-31 22:39:48 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,9 @@
-import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+import streamlit as st
 
-def scrape_products():
+def scrape_articles(url):
     # Send a GET request to the website
-    url = "https://www.example.com/products"
     response = requests.get(url)
     
     # Check if the request was successful
@@ -12,19 +11,35 @@ def scrape_products():
         # Create a BeautifulSoup object to parse the HTML content
         soup = BeautifulSoup(response.content, "html.parser")
         
-        # Find the HTML elements containing the products
-        products = soup.find_all("div", class_="product")
+        # Find the HTML elements containing the articles
+        articles = soup.find_all("h2", class_="article-title")
         
-        # Extract the title and price for each product
-        for product in products:
-            title = product.find("h2").text.strip()
-            price = product.find("span", class_="price").text.strip()
-            
-            print("Title:", title)
-            print("Price:", price)
-            print("------")
+        # Extract the titles of the articles
+        titles = [article.text.strip() for article in articles]
+        
+        return titles
     else:
-        print("Failed to retrieve products.")
+        st.error("Failed to retrieve articles.")
 
-# Call the function to start scraping
-scrape_products()
+# Create a Streamlit app
+def main():
+    st.title("Web Scraping App")
+    
+    # Input URL
+    url = st.text_input("Enter a website URL")
+    
+    if st.button("Scrape"):
+        if url:
+            titles = scrape_articles(url)
+            if titles:
+                st.success("Articles scraped successfully!")
+                st.write("Titles:")
+                for title in titles:
+                    st.write(title)
+            else:
+                st.error("No articles found.")
+        else:
+            st.warning("Please enter a website URL.")
+
+if __name__ == "__main__":
+    main()
