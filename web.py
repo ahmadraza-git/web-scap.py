df41bd24da164ba23327439e7beb155ec5ef12f2
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 00:06:48 -0700

    Update web.py

--- web.py
+++ web.py
@@ -1,20 +1,34 @@
-import streamlit as st
-from bs4 import BeautifulSoup
 import requests
-def scrape_data(url):
+from bs4 import BeautifulSoup
+import streamlit as st
+
+# Function to scrape data from Wikipedia
+def scrape_wikipedia(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.string
-    paragraph = soup.find('p').text
-    return title, paragraph
+    
+    # Extract the desired information from the page
+    title = soup.find('h1', {'id': 'firstHeading'}).text
+    paragraphs = soup.find_all('p')
+    content = '\n\n'.join(p.text for p in paragraphs)
+    
+    return title, content
 
-st.title("Data Scraping App")
-url = st.text_input("Enter the URL to scrape")
+# Streamlit web application
+def main():
+    st.title("Wikipedia Web Scraper")
+    url = st.text_input("Enter Wikipedia URL")
+    
+    if st.button("Scrape"):
+        if url:
+            try:
+                title, content = scrape_wikipedia(url)
+                st.subheader(title)
+                st.markdown(content)
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
+        else:
+            st.warning("Please enter a valid URL.")
 
-if st.button("Scrape"):
-    if url:
-        title, paragraph = scrape_data(url)
-        st.write(f"Title: {title}")
-        st.write(f"Paragraph: {paragraph}")
-    else:
-        st.warning("Please enter a valid URL.")
+if __name__ == '__main__':
+    main()
