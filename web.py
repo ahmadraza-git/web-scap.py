41b78d36e0ecfa357967692fd015bd2cfbba3028
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 00:15:33 -0700

    web.py

--- web.py
+++ web.py
@@ -2,33 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-# Function to scrape data from Wikipedia
-def scrape_wikipedia(url):
+def scrape_websites():
+    url = "https://www.octoparse.com/blog/top-10-most-scraped-websites"
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
     
-    # Extract the desired information from the page
-    title = soup.find('h1', {'id': 'firstHeading'}).text
-    paragraphs = soup.find_all('p')
-    content = '\n\n'.join(p.text for p in paragraphs)
+    website_list = []
+    websites = soup.find_all("h3", class_="post-title")
     
-    return title, content
+    for website in websites:
+        website_list.append(website.text.strip())
 
-# Streamlit web application
+    return website_list
+
 def main():
-    st.title("Wikipedia Web Scraper")
-    url = st.text_input("Enter Wikipedia URL")
-    
-    if st.button("Scrape"):
-        if url:
-            try:
-                title, content = scrape_wikipedia(url)
-                st.subheader(title)
-                st.markdown(content)
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
-        else:
-            st.warning("Please enter a valid URL.")
+    st.title("Top 10 Most Scraped Websites")
+    st.write("Scraping data from https://www.octoparse.com/blog/top-10-most-scraped-websites")
 
-if __name__ == '__main__':
+    websites = scrape_websites()
+
+    if websites:
+        st.write("Here are the top 10 most scraped websites:")
+        for i, website in enumerate(websites, start=1):
+            st.write(f"{i}. {website}")
+    else:
+        st.write("Failed to retrieve website data.")
+
+if __name__ == "__main__":
     main()
