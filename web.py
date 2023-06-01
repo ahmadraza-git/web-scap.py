c9b14bbce03ec8814db310137dbc109dea840483
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 11:17:18 +0500

    Update web.py

--- web.py
+++ web.py
@@ -2,44 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-def scrape_articles(url):
-    # Send a GET request to the website
+
+app = Flask(__name__)
+
+def scrape_data(url):
     response = requests.get(url)
-    
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Create a BeautifulSoup object to parse the HTML content
-        soup = BeautifulSoup(response.content, "html.parser")
-        
-        # Find the HTML elements containing the articles
-        articles = soup.find_all("h2", class_="article-title")
-        
-        # Extract the titles of the articles
-        titles = [article.text.strip() for article in articles]
-        
-        return titles
-    else:
-        st.error("Failed to retrieve articles.")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.title.string
+    paragraph = soup.find('p').text
+    return title, paragraph
 
-# Create a Streamlit app
-def main():
-    st.title("Web Scraping App")
-    
-    # Input URL
-    url = st.text_input("Enter a website URL")
-    
-    if st.button("Scrape"):
+@app.route('/', methods=['GET', 'POST'])
+def scrape():
+    if request.method == 'POST':
+        url = request.form['url']
         if url:
-            titles = scrape_articles(url)
-            if titles:
-                st.success("Articles scraped successfully!")
-                st.write("Titles:")
-                for title in titles:
-                    st.write(title)
-            else:
-                st.error("No articles found.")
+            title, paragraph = scrape_data(url)
+            return render_template('result.html', title=title, paragraph=paragraph)
         else:
-            st.warning("Please enter a website URL.")
+            return "Please enter a valid URL."
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
