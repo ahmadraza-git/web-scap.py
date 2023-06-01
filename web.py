7d5f41a686d01453b51d6fca850c5fbcb9d448c8
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 13:15:17 +0500

    web.py

--- web.py
+++ web.py
@@ -10,14 +10,22 @@ def scrape_data(url):
     if response.status_code == 200:
         # Create a BeautifulSoup object to parse the HTML content
         soup = BeautifulSoup(response.content, 'html.parser')
-        
 
         # Find the content you want to scrape
         content = soup.find('div', {'id': 'mw-content-text'})
 
         # Extract the text from the content
         text = content.get_text()
-        text = content.get_text('bron','died')
+
+        # Find the birth and death information
+        birth = soup.find('span', {'class': 'bday'})
+        death = soup.find('span', {'class': 'dday'})
+
+        birth_text = birth.get_text() if birth else 'N/A'
+        death_text = death.get_text() if death else 'N/A'
+
+        # Add the birth and death information to the scraped text
+        text += f"\n\nBirth: {birth_text}\nDeath: {death_text}"
 
         return text
     else:
