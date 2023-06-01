a5e2f328af08ab9c0eeb131415a43816518e610c
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 00:27:34 -0700

    web.py

--- web.py
+++ web.py
@@ -2,31 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-def scrape_wikipedia(url):
+# Fetch the HTML content of the page
+def fetch_page_content(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+    return response.text
 
-    # Extract the desired information from the web page
-    title = soup.find("h1", id="firstHeading").text.strip()
-    paragraphs = soup.find_all("p")
+# Extract relevant information from the HTML
+def extract_info(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Modify the code below based on the specific information you want to extract
+    # For example, let's extract the page title and the first paragraph
+    title = soup.find('h1', {'id': 'firstHeading'}).text
+    first_paragraph = soup.find('p').text
+    return title, first_paragraph
 
-    content = ""
-    for paragraph in paragraphs:
-        content += paragraph.text.strip() + "\n"
-
-    return title, content
-
+# Main function to run the web scraper
 def main():
-    st.title("Wikipedia Web Scraper")
-    st.write("Scraping data from Wikipedia")
+    st.title("Web Scraper")
+    url = "https://simple.wikipedia.org/wiki/Muhammad_Iqbal"
 
-    url = "https://en.wikipedia.org/wiki/Place"
+    # Fetch the page content
+    html_content = fetch_page_content(url)
 
-    title, content = scrape_wikipedia(url)
+    # Extract relevant information
+    title, first_paragraph = extract_info(html_content)
 
-    st.write(f"Title: {title}")
-    st.write("Content:")
-    st.write(content)
+    # Display the information using Streamlit
+    st.header(title)
+    st.write(first_paragraph)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
