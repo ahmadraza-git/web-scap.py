207f9cf6b8c713b93da5f460573ab32c0b520c77
ahmad raza <126757125+ahmadraza-git@users.noreply.github.com>
2023-06-01 00:43:03 -0700

    web.py

--- web.py
+++ web.py
@@ -1,20 +1,35 @@
-import streamlit as st
-from bs4 import BeautifulSoup
 import requests
-def scrape_data(url):
+from bs4 import BeautifulSoup
+import streamlit as st
+
+# Fetch the HTML content of the page
+def fetch_page_content(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.title.string
-    paragraph = soup.find('p').text
-    return title, paragraph
+    return response.text
 
-st.title("Data Scraping App")
-url = st.text_input("Enter the URL to scrape")
+# Extract relevant information from the HTML
+def extract_info(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Modify the code below based on the specific information you want to extract
+    # For example, let's extract the page title and the first paragraph
+    title = soup.find('h1', {'id': 'firstHeading'}).text
+    first_paragraph = soup.find('p').text
+    return title, first_paragraph
 
-if st.button("Scrape"):
-    if url:
-        title, paragraph = scrape_data(url)
-        st.write(f"Title: {title}")
-        st.write(f"Paragraph: {paragraph}")
-    else:
-        st.warning("Please enter a valid URL.")
+# Main function to run the web scraper
+def main():
+    st.title("Web Scraper")
+    url = "https://simple.wikipedia.org/wiki/Muhammad_Iqbal"
+
+    # Fetch the page content
+    html_content = fetch_page_content(url)
+
+    # Extract relevant information
+    title, first_paragraph = extract_info(html_content)
+
+    # Display the information using Streamlit
+    st.header(title)
+    st.write(first_paragraph)
+
+if __name__ == '__main__':
+    main()
